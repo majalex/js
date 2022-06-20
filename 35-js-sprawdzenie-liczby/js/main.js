dcfc303f7d9453dc2a0946a5c18e882b0f551bb6
Aleksander Maj <majalex@outlook.com>
2022-06-20 15:53:16 +0200

    zadanie

---
+++ 35-js-sprawdzenie-liczby/js/main.js
@@ -0,0 +1,12 @@
+randomNumber = Math.floor(Math.random() * 301);
+
+function number(n) {
+    if (n >= 100 && n <= 200) {
+        return('Liczba znajduje się w przedziale');
+    } else {
+        return('Liczba nie znajduje się w przedziale');
+    }
+}
+
+console.log(randomNumber);
+console.log(number(randomNumber));
