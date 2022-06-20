a124e964409264f62458caad481d08e3bb7717f8
Aleksander Maj <majalex@outlook.com>
2022-06-20 14:17:34 +0200

    poprawki

--- 32-js-wieksza-liczba/js/main.js
+++ 32-js-wieksza-liczba/js/main.js
@@ -3,9 +3,9 @@ let num2 = 5;
 
 function value(num1, num2) {
     if (num1 > num2) {
-        return(num1)
+        return(num1);
     } else if (num1 < num2) {
-        return(num2)
+        return(num2);
     } else {
         return('Liczby są równe');
     }
