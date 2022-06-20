0291cab9274dd6521a8cc10c7f1239a44f9d5789
Aleksander Maj <majalex@outlook.com>
2022-06-20 12:05:44 +0200

    rozwiązania

---
+++ 28-js-warunki/js/main.js
@@ -0,0 +1,7 @@
+let number = 12;
+
+if (number > 10) {
+    console.log('podana liczba jest większa niz 10')
+} else {
+    console.log('podana liczba jest mniejsza, lub równa 10');
+}
---
+++ tablica-zmienne/main.js
@@ -0,0 +1,9 @@
+let score1 = 5;
+let score2 = 6;
+let score3 = 3;
+
+score1 = 1;
+
+let scores = [score1, score2, score3];
+
+console.log(scores);
