c0ccdad140aa234b49423c3c8fc912d69ce21871
Aleksander Maj <majalex@outlook.com>
2022-06-21 11:57:32 +0200

    zadanie, poprawione

--- 38-js-dzialania/js/main.js
+++ 38-js-dzialania/js/main.js
@@ -1,8 +1,24 @@
-function number(a, b) {
-    if (a === 20) {
-        return true;
-    } else if (a + b >= 20)
-        return true;
+function isNegative(number) {
+    if (Math.sign(number) === 1) {
+        return number;
+    } else {
+        return(" wynik jest nieprawidłowy!");
+    }
 }
+function add(num1, num2) {
+    let result = num1 + num2;
+    return result;
+  }
+function subtraction(num1, num2) {
+    let result = num1 - num2;
+    return result;
+  }
+function multiply(num1, num2) {
+    let result = num1 * num2;
+    isNegative(result);
 
-console.log(number(20, 3));
+  }
+
+console.log("1. Wynik dodawania wynosi:", add(5, 7));
+console.log("2. Wynik odejmowania wynosi:", subtraction(5, 7));
+console.log("3. Wynik mnożenia wynosi:", multiply(5, -7));
