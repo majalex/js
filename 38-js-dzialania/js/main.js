8e52bb90fd22f80c93cd9b04f49ab2c85f662a17
Aleksander Maj <majalex@outlook.com>
2022-06-22 22:05:12 +0200

    zadania

--- 38-js-dzialania/js/main.js
+++ 38-js-dzialania/js/main.js
@@ -1,24 +1,27 @@
-function isNegative(number) {
-    if (Math.sign(number) === 1) {
-        return number;
-    } else {
-        return(" wynik jest nieprawidłowy!");
-    }
+function calculator(num1, num2) {
+  let add = num1 + num2;
+  let subtraction = num1 - num2;
+  let multiply = num1 * num2;
+
+  if (add >= 0) {
+    console.log(`1. Wynik dodawania wynosi: ${add}`);
+    // console.log("1. Wynik dodawania wynosi: " + add);
+  } else {
+    console.log("1. Wynik jest nieprawidłowy!");
+  }
+
+  if (subtraction >= 0) {
+    console.log(`2. Wynik odejmowania wynosi: ${subtraction}`);
+    // console.log("1. Wynik dodawania wynosi: " + add);
+  } else {
+    console.log("2. Wynik jest nieprawidłowy!");
+  }
+
+  if (multiply >= 0) {
+    console.log(`3. Wynik mnożenia wynosi: ${multiply}`);
+    // console.log("1. Wynik dodawania wynosi: " + add);
+  } else {
+    console.log(" Wynik jest nieprawidłowy!");
+  }
 }
-function add(num1, num2) {
-    let result = num1 + num2;
-    return result;
-  }
-function subtraction(num1, num2) {
-    let result = num1 - num2;
-    return result;
-  }
-function multiply(num1, num2) {
-    let result = num1 * num2;
-    isNegative(result);
-
-  }
-
-console.log("1. Wynik dodawania wynosi:", add(5, 7));
-console.log("2. Wynik odejmowania wynosi:", subtraction(5, 7));
-console.log("3. Wynik mnożenia wynosi:", multiply(5, -7));
+  console.log(calculator(3, 8));
---
+++ 40-js-dane-formularz/js/main.js
@@ -0,0 +1,15 @@
+// let input1 = document.getElementById('fname').value;
+// console.log(input1);
+// let par1 = document.getElementById('name');
+
+
+document.querySelector('button').onclick = function () {
+  let input1 = document.getElementById('fname').value;
+  console.log(input1);
+  let par1 = document.getElementById('name');
+  par1.innerText = input1;
+}
+
+
+
+
