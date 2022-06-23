e5894b6987bb99aaa76434bfd42972576cd243f3
Aleksander Maj <majalex@outlook.com>
2022-06-23 11:33:16 +0200

    zadanie

--- 40-js-dane-formularz/js/main.js
+++ 40-js-dane-formularz/js/main.js
@@ -1,15 +1,18 @@
-// let input1 = document.getElementById('fname').value;
-// console.log(input1);
-// let par1 = document.getElementById('name');
-
-
 document.querySelector('button').onclick = function () {
+
   let input1 = document.getElementById('fname').value;
   console.log(input1);
   let par1 = document.getElementById('name');
   par1.innerText = input1;
+
+  let input2 = document.getElementById('lname').value;
+  console.log(input2);
+  let par2 = document.getElementById('surname');
+  par2.innerText = input2;
+
+  let input3 = document.getElementById('number').value;
+  console.log(input3);
+  let par3 = document.getElementById('phone');
+  par3.innerText = input3;
+  
 }
-
-
-
-
